8c32a764e56efc9431edaee3d64144166dc81782
maxgertzen <maxgertzen@gmail.com>
2021-06-28 14:30:38 +0300

    switched to readling-sync package and finalized phase1

--- index.js
+++ index.js
@@ -1,74 +1,41 @@
-const readline = require("readline");
+const readline = require('readline-sync');
 
-let rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const randomizer = (min = -5, max = 13) => {
+    return Math.floor((Math.random() * (max - min) + min));
+}
 
-function EvenOdd(round) {
-    this.round = round;
-    const users = [];
-    let numberOfPlayers = null;
-    let winner = null;
+function EvenOddGame() {
+    const players = [
+        {
+            name: readline.question('1st Player Name: \n'),
+            score: 0
+        },
+        {
+            name: readline.question('2nd Player Name: \n'),
+            score: 0
+        }]
 
+    const rounds = readline.question('How many rounds?\n');
+    const scoreToWin = Math.floor(rounds / 2) + 1;
 
-
-    // let num = __randomizer();
-    // if (num % 2 === 0) {
-    //     users[0]['score'] += 1
-    //     console.log(`${users[0]['name']} won this round`)
-    // } else {
-    //     users[1]['score'] += 1
-    //     console.log(`${users[1]['name']} won this round`)
-    // }
-
-    // if (users[0]['score'] > Math.floor(5 / 2)) {
-    //     winner = users[0]
-    //     rl.close();
-    //     break
-    // }
-    // else if (users[1]['score'] > Math.floor(5 / 2)) {
-    //     winner = users[1]
-    //     rl.close()
-    //     break
-    // }
-    // rl.on('close', function () {
-    //     console.log(`${winner} has won with ${users[winner].score} points`);
-    //     process.exit(0);
-    // })
-
-
-    function start() {
-        rl.question("How many players will play ? \n", async function (number) {
-            if (Math.round(number.trim()) < 2 || Math.round(number.trim()) > 7) {
-                console.log("\nNumber of players allowed is between 2-7")
-                rl.close()
-            } else {
-                numberOfPlayers = number.trim()
-            }
-        });
-        __getNames();
-    }
-
-    async function __getNames() {
-        for (let i = 0; i < numberOfPlayers; i++) {
-            rl.question(`Player${i + 1} name:\n`, function (name) {
-                users.push({ name, score: 0 });
-                rl.close()
-            })
+    const addScore = (user, randomNumber) => {
+        user.score++;
+        console.log(`${user.name} is winner of round ${round}, after drawing ${randomNumber} random number.\n1 score added.`)
+        if (user.score === scoreToWin) {
+            console.log(`${user.name} won the game`)
         }
-        __runGame();
     }
 
-    function __runGame() {
-        console.log('run')
-    }
-
-    return {
-        start
+    let round = 1;
+    while (players[0].score < scoreToWin && players[1].score < scoreToWin) {
+        let rand = randomizer();
+        if (rand % 2 === 0) {
+            addScore(players[0], rand)
+        } else {
+            addScore(players[1], rand)
+        }
+        round++;
     }
 }
 
-let game = new EvenOdd(5);
-
-game.start()
+let game = new EvenOddGame();
--- module.js
+++ module.js
@@ -1,19 +1,7 @@
-const argv = require("minimist")(process.argv.slice(2));
-
-function User(name) {
-    this.name = name;
-    this.score = 0;
-}
-
-
 const randomizer = (min = -5, max = 13) => {
     return Math.floor((Math.random() * (max - min) + min));
 }
 
-const declareWinner = (...users) => users.find((player) => player.score === 3);
-
 module.exports = {
-    User,
-    randomizer,
-    declareWinner
+    randomizer
 }
